ac357c340629576bd758d68aa7fa6a5dc6105efe
Jose Pirela <josepirela160589@gmail.com>
2023-06-13 11:53:35 -0400

    Cambios en crearActividad

--- src/scenes/voluntariado/crearActividad.jsx
+++ src/scenes/voluntariado/crearActividad.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Breadcrumbs from '@mui/material/Breadcrumbs';
 import Card from '@mui/material/Card';
@@ -12,8 +15,9 @@ import Select from '@mui/material/Select';
 import OutlinedInput from '@mui/material/OutlinedInput';
 import TextField from '@mui/material/TextField';
 import { LocalizationProvider } from '@mui/x-date-pickers';
+//import { esES } from '@mui/x-date-pickers/locales';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
 import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker';
 import 'dayjs/locale/es';
 import Tooltip from '@mui/material/Tooltip';
@@ -21,13 +25,14 @@ import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
 import Checkbox from '@mui/material/Checkbox';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Link from '@mui/material/Link';
 import HomeIcon from '@mui/icons-material/Home';
 import DeleteIcon from '@mui/icons-material/Delete';
 import VolunteerActivismIcon from '@mui/icons-material/VolunteerActivism';
-import ScheduleIcon from '@mui/icons-material/Schedule';
+import MoreTimeIcon from '@mui/icons-material/MoreTime';
 
 // Array Select Multiple
 const actividad = [
@@ -50,14 +55,56 @@ const equipos = [
 
 const CrearActividad = () => {
 
-//Theme
-const theme = useTheme();
+ const nav = useNavigate();
+
+ //Theme
+ const theme = useTheme();
+
+ //DatePicker Value
+ const [valueDP, setValueDP] = React.useState(new Date());
+ const handleChangeDP = (newValueDP) => {
+  setValueDP(newValueDP);
+};
+
+ //TimePicker Values
+ const [newValueHE, setValueHE] = React.useState(null);
+ const handleChangeHE = (newValueHE) => {
+  setValueHE(newValueHE);
+};
+
+ const [newValueHS, setValueHS] = React.useState(null);
+ const handleChangeHS = (newValueHS) => {
+  setValueHS(newValueHS);
+};
+
+const [newValueRT, setValueRT] = React.useState(null);
+const handleChangeRT = (newValueRT) => {
+ setValueRT(newValueRT);
+};
 
 //BreadCrumbs
 function handleClickBreadCrumbs(event) {
   event.preventDefault();
   console.info('You clicked a breadcrumb.');
 }
+
+//Modal Styles
+const style = {
+  position: 'inherit',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 375,
+  bgcolor: theme.palette.background.alt,
+  border: '2px solid #000',
+  borderRadius: 4,
+  boxShadow: 1,
+  p: 4, 
+};
+
+const [open, setOpen] = React.useState(false);
+const handleOpen = () => setOpen(true);
+const handleClose = () => setOpen(false);
 
 //Actividad Select
 const [actividadOpc, setActividadOpc] = React.useState([]);
@@ -107,8 +154,49 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
         setData(deleteVal)
     }
 
+//Routes
+const handleLinkClick = (event, message) => {
+  if (message === 'cancelar') {
+    nav("/voluntariado") 
+  }
+};
+const handleLinkHome = (event, message) => {
+  if (message === 'home') {
+    nav("/inicio")
+  }
+};
+const handleLinkVol = (event, message) => {
+  if (message === 'voluntariado') {
+    nav("/voluntariado")
+  }
+};
+
+//Form Submit
+const [loc, setLoc] = React.useState('');
+
+const handleLoc = (event) => {
+  setLoc(event.target.value);
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  alert(proyecto + ' ' + actividadOpc + ' ' + loc + ' ' + valueDP);
+};
+
   return (
-    <Box m="2.5rem 2.5rem">
+    <Box m="1.5rem 1.5rem">
+    <Box
+    gridColumn="span 2"
+    gridRow="span 1"
+    display="flex"
+    flexDirection="column"
+    justifyContent="space-between"
+    p="1.25rem 1rem"
+    flex="1 1 100%"
+    borderRadius="0.55rem"
+    >
+    <CssBaseline />
     <Box role="presentation" onClick={handleClickBreadCrumbs} sx={{ mb:3 }}>
     <Breadcrumbs aria-label="breadcrumb">
     <Link
@@ -116,6 +204,7 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
       sx={{ display:'flex', alignItems:'center' }}
       color="inherit"
       href="/inicio"
+      onClick={event => handleLinkHome(event, 'home')}
     >
       <HomeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
       Inicio
@@ -125,6 +214,7 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
       sx={{ display:'flex', alignItems:'center' }}
       color="inherit"
       href="/voluntariado"
+      onClick={event => handleLinkVol(event, 'voluntariado')}
     >
       <VolunteerActivismIcon sx={{ mr: 0.5 }} fontSize="inherit" />
       Voluntariado
@@ -133,26 +223,26 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
       sx={{ display:'flex', alignItems:'center' }}
       color="text.primary"
     >
-      <ScheduleIcon sx={{ mr: 0.5 }} fontSize="inherit" />
+      <MoreTimeIcon sx={{ mr: 0.5 }} fontSize="inherit" />
        Crear Convocatoria
     </Typography>
   </Breadcrumbs>
+  <Typography variant="h3" sx={{ fontWeight:'bold', mt:2 }}> Crear Convocatoria </Typography>
   </Box> 
+  <Container>
   <Card sx={{ borderRadius:8, boxShadow:5, bgcolor:theme.palette.background.alt }}>
     <CardContent>
-      <Typography variant="h4" component="div" sx={{ ml:2, mb:3, mt:3, fontWeight:'bold' }}>
-        Nueva Convocatoria
-      </Typography>
       <Divider sx={{ mt:2, mb:3 }}>
         <Chip label="INFORMACIÓN GENERAL" />
       </Divider>
+      <form onSubmit={handleSubmit}>
       <Grid container spacing={2} mt={1}>
         <Grid item xs={12} sm={4}>
           <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label"> Proyecto que Convoca </InputLabel>
+            <InputLabel id="demo-simple-select-label" required> Proyecto que Convoca </InputLabel>
               <Select
                 labelId="demo-simple-select-label"
-                id="demo-simple-select"
+                id="convoc"
                 label="Proyecto que Convoca"
                 value={proyecto}
                 onChange={handleChangeProyecto}
@@ -168,10 +258,10 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
         </Grid>
         <Grid item xs={12} sm={4}>
          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label"> Tipo de Actividad </InputLabel>
+            <InputLabel id="demo-simple-select-label" required> Tipo de Actividad </InputLabel>
               <Select
                 labelId="demo-simple-select-label"
-                id="demo-simple-select"
+                id="tipoAct"
                 label="Tipo de Actividad"
                 multiple
                 value={actividadOpc}
@@ -196,15 +286,22 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
           fullWidth
           id="locacion"
           label="Locación"
+          value={loc}
+          onChange={handleLoc}
           />
         </Grid>
         <Grid item xs={12} sm={4}>
           <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es">  
-            <DatePicker 
-            name="fecha"
+            <DesktopDatePicker
             label="Fecha"
-            format="DD-MM-YYYY"
-            sx={{ width: '100%' }}
+            name="fecha"
+            inputFormat="DD/MM/YYYY" 
+            value={valueDP}
+            onChange={handleChangeDP}
+            renderInput={(params) => {
+              return <TextField  fullWidth {...params} />;
+            }}
+            clearable
             />
           </LocalizationProvider>
         </Grid>
@@ -227,32 +324,40 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
         />
         </Grid>
         <Grid item xs={12} sm={4}>
-          <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
             <MobileTimePicker
             name="horaEncuentro"
-            label="Hora de Encuentro" 
-            sx={{ width: '100%'}}
+            label="Hora de Encuentro"
+            value={newValueHE}
+            onChange={handleChangeHE}
+            renderInput={(params) => {
+              return <TextField fullWidth {...params} />;
+            }}
             />
           </LocalizationProvider>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
             <MobileTimePicker
             name="horaSalida"
             label="Hora de Salida" 
-            sx={{ width: '100%' }}
+            value={newValueHS}
+            onChange={handleChangeHS}
+            renderInput={(params) => <TextField fullWidth {...params} />}
             />
           </LocalizationProvider>
         </Grid>
         <Grid item xs={12} sm={4}>
-          <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="es">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
             <MobileTimePicker
-            name="horaSalida"
+            name="horaRetorno"
             label="Hora Estimada de Retorno" 
-            sx={{ width: '100%' }}
+            value={newValueRT}
+            onChange={handleChangeRT}
+            renderInput={(params) => <TextField fullWidth {...params} />}
             />
           </LocalizationProvider>
-        </Grid>
+          </Grid>
       </Grid>
       <Divider sx={{ mt:4, mb:3 }}>
         <Chip label="CANTIDAD DE VOLUNTARIOS" />
@@ -262,14 +367,25 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
           <div>
           <Grid container spacing={2} sx={{ mt:1 }} justifyContent="center">
             <Grid item xs={12} sm={4}>
-              <TextField
-                required
-                fullWidth
-                id="roles"
+              <FormControl fullWidth>
+               <InputLabel id="roles" required> Roles </InputLabel>
+               <Select
+                labelId="roles-select"
+                id="roles-select"
                 label="Roles"
-                name="roles" 
-                value={val.roles} onChange={(e)=>handleChangeInput(e,i)}
-              />
+                name='roles'
+                value={val.roles}
+                onChange={(e)=>handleChangeInput(e,i)}
+               >
+                 <MenuItem value="">
+                   <em> Seleccione un Rol </em>
+                 </MenuItem>
+                 <MenuItem value={'Médico General'}> Médico General </MenuItem>
+                 <MenuItem value={'Médico Familiar'}> Médico Familiar </MenuItem>
+                 <MenuItem value={'Ginecólogo'}> Ginecólogo </MenuItem>
+                 <MenuItem value={'Internista'}> Internista </MenuItem>
+               </Select>
+              </FormControl>
             </Grid>
             <Grid item xs={12} sm={4}>
               <TextField
@@ -281,11 +397,12 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
                 value={val.metas} onChange={(e)=>handleChangeInput(e,i)}
               />
             </Grid>
-            <Grid item xs={12} sm={1} sx={{ mt: 1 }}>
+            <Grid item xs={4} sm={1} sx={{ mt: 1 }}>
               <Tooltip title="Eliminar">
                 <Button variant='contained' 
                 color='error' 
                 fullWidth 
+                sx={{ borderRadius:2 }}
                 onClick={()=>handleDelete(i)}
                 > 
                   <DeleteIcon /> 
@@ -297,9 +414,9 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
             )
           }
           <Grid container justifyContent="center">
-          <Grid item xs={12} sm={2} sx={{ mt:3 }}>
-            <Button variant='contained' sx={{ bgcolor:'teal', color:'white' }} fullWidth onClick={handleClick}> Añadir Más </Button>
-          </Grid>
+            <Grid item xs={6} sm={2} sx={{ mt:3 }}>
+              <Button variant='contained' sx={{ bgcolor:'teal', color:'white', borderRadius:2 }} fullWidth onClick={handleClick}> Añadir Más </Button>
+            </Grid>
           </Grid>
           {/*<p>{JSON.stringify(data)}</p>*/}
           <Divider sx={{ mt:4, mb:3 }}>
@@ -370,7 +487,8 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
               fullWidth
               id="comentarios"
               label="Comentarios"
-              rows={4}
+              multiline
+              maxRows={4}
               />
             </Grid>
           </Grid>
@@ -378,7 +496,7 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
             <Chip label="INSUMOS NECESARIOS" />
           </Divider>
           <Grid container justifyContent="center">
-            <Grid item xs={12} sm={7}>
+            <Grid item xs={12} sm={8}>
             <div>
           <FormControlLabel
             control={
@@ -416,14 +534,37 @@ const [data,setData]=useState([{roles:"",cantidad:""}])
           <Divider sx={{ mt:5 }}></Divider>
           <Grid container sx={{ mt:2 }} spacing={1} justifyContent="center" >
             <Grid item sm={3} xs={6}>
-              <Button variant='contained' color='error' size='large' fullWidth> Cancelar </Button>
+              <Button variant='contained' color='error' size='large' fullWidth onClick={handleOpen}> Cancelar </Button>
             </Grid>
             <Grid item sm={3} xs={6}>
-              <Button variant='contained' sx={{ bgcolor:'teal', color:'white' }} size='large' fullWidth> Guardar </Button>
+              <Button variant='contained' type='submit' sx={{ bgcolor:'teal', color:'white' }} size='large' fullWidth> Guardar </Button>
             </Grid>
           </Grid>
+        </form>
     </CardContent>
   </Card>
+    <Modal
+    open={open}
+    onClose={handleClose}
+    aria-labelledby="modal-modal-title"
+    aria-describedby="modal-modal-description"
+    slotProps={{ backdrop: { style: { opacity: 0.2 } } }}
+    >
+      <Box sx={style}>
+        <Typography variant='h6' className='text-center' sx={{ mb:1.5 }}> ¿Está seguro de cancelar esta operación? </Typography>
+          <Divider sx={{ mt:2 }} />
+          <Grid container sx={{ mt:1 }} spacing={1} justifyContent="flex-end" >
+             <Grid item>
+               <Button variant='contained' color='error' onClick={handleClose}> No </Button>
+             </Grid>
+             <Grid item>
+               <Button variant='contained' sx={{ bgcolor:'teal', color:'white' }} onClick={event => handleLinkClick(event, 'cancelar')}> Si </Button>
+             </Grid>
+          </Grid>
+        </Box>
+    </Modal>
+  </Container>
+  </Box>
   </Box>
   )
 }
