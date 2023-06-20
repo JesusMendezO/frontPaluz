fa1603fd937ca2d610fa9925c2282be91350d648
japirela <126787080+japirela@users.noreply.github.com>
2023-06-20 11:43:47 -0400

    Update StatChart.jsx

--- src/components/StatChart.jsx
+++ src/components/StatChart.jsx
@@ -666,7 +666,7 @@ const StatChart = () => {
         axisBottom={{
             tickSize: 5,
             tickPadding: 5,
-            tickRotation: 0,
+            tickRotation: -45,
             legend: 'transportation',
             legendOffset: 36,
             legendPosition: 'middle'
@@ -674,7 +674,7 @@ const StatChart = () => {
         axisLeft={{
             tickSize: 5,
             tickPadding: 5,
-            tickRotation: -45,
+            tickRotation: 0,
             legend: 'count',
             legendOffset: -40,
             legendPosition: 'middle'
