a35d7fdba6675e4b9ca36d704023bf68cce474cf
Jose Pirela <josepirela160589@gmail.com>
2023-06-29 09:28:12 -0400

    AgregarMiembros agregado

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Layout from "scenes/layout";
 import Login from "scenes/login";
 import Registro from "scenes/register";
 import ResetPass from "scenes/resetPass";
+import CreatePass from "scenes/createPass";
 import Dashboard from "scenes/dashboard";
 import Proyectos from "scenes/proyectos";
 import Beneficiarios from "scenes/beneficiarios";
@@ -19,17 +20,21 @@ import Suministros from "scenes/suministros";
 import GestionHumana from "scenes/gestionhumana";
 import Admin from "scenes/admin";
 import Upload from "scenes/cargar-datos";
+import Actividades from "scenes/proyectos/actividades";
 import CrearProyecto from "scenes/proyectos/crearProyecto";
 import AdminEquipos from "scenes/proyectos/adminEquipos";
+import AgregarMiembros from "scenes/proyectos/agregarMiembros";
 import GestionProyectos from "scenes/proyectos/gestionProyectos";
 import Estadisticas from "scenes/proyectos/estadisticas";
 import CrearActividad from "scenes/voluntariado/crearActividad";
 import AceptarUsuarios from "scenes/admin/aceptarUsuarios";
-import CreatePass from "scenes/createPass";
-import Overview from "scenes/overview";
+import Logros from "scenes/voluntariado/logros";
+import EasterEgg from "scenes/easterEgg";
+
 function App() {
   const mode = useSelector((state) => state.global.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
   return (
     <div className="app">
       <BrowserRouter>
@@ -40,6 +45,7 @@ function App() {
             <Route path="/registro" element={<Registro />} />
             <Route path="/resetPass" element={<ResetPass />} />
             <Route path="/createPass" element={<CreatePass />} />
+            <Route path="/secret" element={<EasterEgg />} />
             <Route element={<Layout />}>
               {/*<Route path="/" element={<Navigate to="/inicio" replace />} />*/}
               <Route path="/inicio" element={<Dashboard />} />
@@ -52,12 +58,14 @@ function App() {
               <Route path="/admin" element={<Admin />} />
               <Route path="/crearProyecto" element={<CrearProyecto />} />
               <Route path="/adminEquipos" element={<AdminEquipos />} />
+              <Route path="/agregarMiembros" element={<AgregarMiembros />} />
               <Route path="/gestionProyectos" element={<GestionProyectos />} />
               <Route path="/estadisticas" element={<Estadisticas />} />
+              <Route path="/actividades" element={<Actividades />} />
               <Route path="/crearConvocatoria" element={<CrearActividad />} />
+              <Route path="/logros" element={<Logros />} />
               <Route path="/aceptarUsuarios" element={<AceptarUsuarios />} />
               <Route path="/cargar-datos" element={<Upload />} />
-              <Route path="/overview" element={<Overview />} />
             </Route>
           </Routes>
         </ThemeProvider>
